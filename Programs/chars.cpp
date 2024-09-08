89ad9d87a678fb65dc053f09da24ad96175fd704
Russell Longley <rlongley@gmail.com>
2024-09-08 16:41:00 -0500

    cpp printouts

---
+++ Programs/chars.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+
+int year;	//initialize variable
+year =0;	//sets value
+//int year = 0 is the same as above
+
+std::cout << "Enter year: ";
+std::cin >> year;
+std::cout << "It is the year "<< year <<" and zombies rule the planet\n"; //print variable value
+
+
+}
---
+++ Programs/hello2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+  
+  std::cout << "Hello World2!\n";
+  std::cout << "can I do two lines?\n";
+  std::cout << "i can!\n";
+
+}
