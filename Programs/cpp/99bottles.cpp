dcb909f90537865c5ba26a1ea70d8b91a31f8634
root <root@RussPC.>
2024-09-13 21:49:29 -0500

    for

---
+++ Programs/cpp/99bottles.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+
+  // Write a for loop here:
+  /*i bottles of pop on the wall.
+Take one down and pass it around.
+i-1 bottles of pop on the wall.*/
+for (int i=99;i>0;i--)
+{
+    std::cout << i << " bottles of pop on the wall.\nTake one down and pass it around.\n " << i-1 << " bottles of pop on the wall\n";
+}
+  
+   
+}
