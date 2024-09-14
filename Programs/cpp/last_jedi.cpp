c56275e775ed4fa57785e10230baa7eb6093ea9d
root <root@RussPC.>
2024-09-14 10:19:35 -0500

    random, vectors

---
+++ Programs/cpp/last_jedi.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+  
+  std::vector<std::string> last_jedi;
+  
+  // Add characters here:
+  
+  last_jedi.push_back("kylo");
+  last_jedi.push_back("rey");
+  last_jedi.push_back("luke");
+  last_jedi.push_back("finn");
+
+  std::cout << last_jedi[0] << "\n";
+  std::cout << last_jedi[1] << "\n";
+  std::cout << last_jedi[2] << "\n";
+  std::cout << last_jedi[3] << "\n";
+  
+}
---
+++ Programs/cpp/subway.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+   
+  std::vector<double> subway_adult = {800, 1200, 1500};
+  
+  // Give subway_child some values:
+  
+  std::vector<double> subway_child = {400, 600, 750};
+  
+  std::cout << "3rd value (index 2) = " << subway_child[2] << "\n";
+
+}
