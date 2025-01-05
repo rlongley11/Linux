dca1c1427a625391bfef2a7f61a362a74079c1d6
root <root@RussPC.>
2025-01-04 18:27:43 -0600

    reorganize

--- Programs/bash/test.sh
+++ Programs/bash/test.sh
@@ -3,9 +3,6 @@
 echo
 echo "contents of rlongley11 folder"
 ls /home/rlongley11/ | tee test.txt
-(echo
-echo "contents of home"
-ls /home/
-echo
-echo "gitdirty"
-../../gitdirty.sh) | tee -a test.txt
+(ls /home/
+
+/home/rlongley11/gitdirty) | tee -a test.txt
